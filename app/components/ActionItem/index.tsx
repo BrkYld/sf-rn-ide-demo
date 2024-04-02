8e3fa5ca477ba44a741f53900582e98af6e64d42
ide <idedemo@smartface.io>
2024-04-02 21:38:53 +0000

    action item

--- app/components/ActionItem/index.tsx
+++ app/components/ActionItem/index.tsx
@@ -1,12 +1,11 @@
 import React from "react";
-import Avatar from "@app/components/Avatar";
 import { useContext } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ImageStyle, Image } from "react-native";
 import { ThemeContext } from "@app/context";
 type IComponentProps = {
     style?: any;
 };
 export default (props: any) => {
     const { styles } = useContext(ThemeContext);
-    return (<View style={[styles[".view"], styles[".navItem"]]} left={258.54} top={685.37}><Avatar key="avatar" style={[styles[".view"], styles[".avatar"]]}/><Text key="text1" style={[styles[".text"], styles[".actionItem-text1"]]}>{"Action"}</Text></View>);
+    return (<View style={[styles[".view"], styles[".navItem"]]} left={258.54} top={685.37}><View key="avatarContainer" style={[styles[".view"], styles[".avatar"]]}><Image key="imgAvatar" style={[styles[".image"], styles[".avatar-image1"]] as ImageStyle} source={require("@app/assets/images/garanti.png")}/></View><Text key="text1" style={[styles[".text"], styles[".actionItem-text1"]]}>{"Action"}</Text></View>);
 };
